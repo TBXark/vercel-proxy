feaff933a43dbea1ef96762f9e444b2604856517
tbxark <tbxark@outlook.com>
2024-08-29 16:19:23 +0800

    chore: 添加错误日志

--- api/index.go
+++ api/index.go
@@ -1,42 +1,62 @@
 package api
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"regexp"
 	"strings"
 )
 
+func internalServerError(w http.ResponseWriter, err error) {
+	if err != nil {
+		log.Printf("Internal server error: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("WithHandler panic: %v", err)
+			http.Error(w, fmt.Sprintf("internal server error: %v", err), http.StatusInternalServerError)
+		}
+	}()
+
+	// Set the CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-PROXY-HOST, X-PROXY-SCHEME")
 
+	// Handle the OPTIONS preflight request
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
 
+	// Redirect to the GitHub repository
 	if r.URL.Path == "/" {
 		http.Redirect(w, r, "https://github.com/TBXark/vercel-proxy", http.StatusMovedPermanently)
 		return
 	}
 
-	re := regexp.MustCompile(`^/{0,}(https?:)/{0,}`)
+	// Get the URL to proxy
+	re := regexp.MustCompile(`^/*(https?:)/*`)
 	u := re.ReplaceAllString(r.URL.Path, "$1//")
 	if r.URL.RawQuery != "" {
 		u += "?" + r.URL.RawQuery
 	}
-
 	if !strings.HasPrefix(u, "http") {
 		http.Error(w, "invalid url: "+u, http.StatusBadRequest)
 		return
 	}
 
+	// Create a new request
 	req, err := http.NewRequest(r.Method, u, r.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		internalServerError(w, err)
 		return
 	}
 	for k, v := range r.Header {
@@ -45,21 +65,32 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Send the request to the real server
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		internalServerError(w, err)
 		return
 	}
-	defer resp.Body.Close()
+	defer func(writer http.ResponseWriter, response *http.Response) {
+		internalServerError(writer, response.Body.Close())
+	}(w, resp)
+
+	// Copy the response headers to the output stream
 	for k, v := range resp.Header {
 		for _, vv := range v {
 			w.Header().Add(k, vv)
 		}
 	}
-	w.Header().Del("Referer")
-	// 将Referer的值替换为请求的Host
-	w.Header().Add("Referer", r.Host)
-	// w.Header().Add("X-Requested-Host", r.Host)
-	w.WriteHeader(resp.StatusCode)
+	if w.Header().Get("Referer") != "" {
+		w.Header().Del("Referer")
+		w.Header().Add("Referer", r.Host)
+	}
+
+	// Copy the response body to the output stream
 	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		internalServerError(w, err)
+		return
+	}
+	w.WriteHeader(resp.StatusCode)
 }
